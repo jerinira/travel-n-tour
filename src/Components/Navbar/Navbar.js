2d69c5bfac1504c32f5a2797ed0564c95ef08778
jerinira <jerin00177@gmail.com>
2021-02-27 18:40:22 +0100

    button added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,27 @@
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react'
+import { Button } from './Button/Button';
 import { Link } from 'react-router-dom'
+import './Navbar.css';
 
 function Navbar() {
     const [click,setClick]= useState(false);
+    const [button,setButton]=useState(true);
     const handleClick=()=>{setClick(!click);}
+    const closeMobileMenu=()=>setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+          setButton(false);
+        } else {
+          setButton(true);
+        }
+      };
+    
+      useEffect(() => {
+        showButton();
+      }, []);
+    
+      window.addEventListener('resize', showButton);
     return (
         <nav className="navbar">
             <div className="navbar-container">
@@ -13,6 +31,21 @@ function Navbar() {
                 <div className="menu-icon" onClick={handleClick}>
                     <i className={click? 'fas fa-times' : 'fas fa-bars'}></i>
                 </div>
+                <ul className={click?'nave-menu active' : 'nav-menu'}>
+                    <li className="nav-item">
+                        <Link to="/home" className="nav-links" onClick={closeMobileMenu}>Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/service" className="nav-links" onClick={closeMobileMenu}>Services</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/product" className="nav-links" onClick={closeMobileMenu}>Products</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>Sign-Up</Link>
+                    </li>
+                </ul>
+                {button && <Button buttonStyle='btn--outline'>Sign-in</Button>}
             </div>
         </nav>
             
